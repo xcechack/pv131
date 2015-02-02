db6c4b80cc8219b6ffaedd8bbe2800581b45fe59
unknown <Uivatel@Janicka.local>
2015-02-02 18:15:46 +0100

    pickUp objects added, no functionality yet - in progress

---
+++ UnityProject/Assets/Standard Assets/Character Controllers/Sources/Scripts/PickUpRotation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpRotation : MonoBehaviour {
+
+	void Start () {
+	
+	}
+
+	void Update() {
+		// Slowly rotate the object around axis at 180 degree/second.
+		transform.Rotate(Vector3.down * Time.deltaTime*180);
+	}
+
+	void onTriggerEnter(Collider other){
+		Destroy (other.gameObject);
+	}
+}
